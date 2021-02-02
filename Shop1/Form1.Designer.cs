5cd2b0849bd8ca31af394a083b3ee6ad043d84fc
theacidday <stanisyan16@gmail.com>
2021-02-02 15:02:18 +0200

    Добавьте файлы проекта.

---
+++ Shop1/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace Shop1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Labelbalance = new System.Windows.Forms.Label();
+            this.textBoxAddBalance = new System.Windows.Forms.TextBox();
+            this.buttonAddBalance = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(81, 110);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Баланс";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(35, 178);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(111, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Пополнить баланс";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(276, 173);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(14, 15);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "р";
+            // 
+            // Labelbalance
+            // 
+            this.Labelbalance.AutoSize = true;
+            this.Labelbalance.Location = new System.Drawing.Point(201, 110);
+            this.Labelbalance.Name = "Labelbalance";
+            this.Labelbalance.Size = new System.Drawing.Size(31, 15);
+            this.Labelbalance.TabIndex = 4;
+            this.Labelbalance.Text = "1000";
+            // 
+            // textBoxAddBalance
+            // 
+            this.textBoxAddBalance.Location = new System.Drawing.Point(176, 170);
+            this.textBoxAddBalance.Name = "textBoxAddBalance";
+            this.textBoxAddBalance.Size = new System.Drawing.Size(100, 23);
+            this.textBoxAddBalance.TabIndex = 5;
+            // 
+            // buttonAddBalance
+            // 
+            this.buttonAddBalance.Location = new System.Drawing.Point(154, 222);
+            this.buttonAddBalance.Name = "buttonAddBalance";
+            this.buttonAddBalance.Size = new System.Drawing.Size(122, 23);
+            this.buttonAddBalance.TabIndex = 6;
+            this.buttonAddBalance.Text = "Пополнить баланс";
+            this.buttonAddBalance.UseVisualStyleBackColor = true;
+            this.buttonAddBalance.Click += new System.EventHandler(this.buttonAddBalance_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(712, 450);
+            this.Controls.Add(this.buttonAddBalance);
+            this.Controls.Add(this.textBoxAddBalance);
+            this.Controls.Add(this.Labelbalance);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label balance;
+        private System.Windows.Forms.TextBox textBoxAddBalance;
+        private System.Windows.Forms.Button buttonAddBalance;
+        protected System.Windows.Forms.Label Labelbalance;
+    }
+}
+
---
+++ Shop1/Form1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Shop1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonAddBalance_Click(object sender, EventArgs e)
+        {
+            int balance = Convert.ToInt32(Labelbalance.Text);
+            balance += Convert.ToInt32(textBoxAddBalance.Text);
+            Labelbalance.Text = balance.ToString();
+            textBoxAddBalance.Clear();
+        }
+    }
+}
